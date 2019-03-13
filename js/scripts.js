3503d7ab9cb86b088456509e84412fe71545e0db
JK-09 <47397820+JK-09@users.noreply.github.com>
2019-03-13 19:54:48 +0000

    correction of form
    removal of form in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,2 @@
 $("#p").hover(function(event) {
 });
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
