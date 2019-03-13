a8bcabda198d5b88cc14db96638ea7de6259f7ff
hannah <hannahchristina97@hotmail.co.uk>
2019-03-13 19:16:13 +0000

    Revert "rocket added"
    This reverts commit 5b7041c0c19a12ffef78a6a80a57cb78b6848a72.

--- js/scripts.js
+++ js/scripts.js
@@ -7,18 +7,3 @@ function openForm() {
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-
-const star = document.querySelector('.star');
-const stars = document.querySelector('.stars');
-
-for(let i = 0; i <= 40; i++) {
-  const newStar = star.cloneNode();
-  const top = getRandomInt(100);
-  const left = getRandomInt(100);
-  const size = getRandomInt(20) / 200;
-  newStar.setAttribute('style', `top: ${top}%; left: ${left}%; width: ${size}em; height: ${size}em`);
-  stars.appendChild(newStar);
-}
