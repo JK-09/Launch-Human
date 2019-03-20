b806fe22c992001fb9c40841743b8c7ccb6066fc
nmahfooz <47188105+nmahfooz@users.noreply.github.com>
2019-03-20 18:59:56 +0000

    update

--- js/scripts.js
+++ js/scripts.js
@@ -29,21 +29,3 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
-
-var slideIndex = 0;
-showSlides();
-=======
-// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
-window.onscroll = function() {scrollFunction()};
-
-function showSlides() {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
