8d0db9848dccd4ebaa5bd042af9a0f30710d666b
hannah <hannahchristina97@hotmail.co.uk>
2019-03-20 19:00:36 +0000

    tidying

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,12 @@ function scrollFunction() {
     document.getElementById("logo").style.fontSize = "35px";
   }
 }
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
